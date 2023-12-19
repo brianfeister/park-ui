e9fbb063c4f5ddd25061f1599569968c1dee672f
Brian Feister <bfeister@salesforce.com>
2023-12-19 10:20:53 -0700

    Attempt to add `DatePicker` component from Ark-UI to solid js  solid-start project

--- examples/panda/solid/solid-start/src/routes/index.tsx
+++ examples/panda/solid/solid-start/src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { Title } from 'solid-start'
 import { Container } from 'styled-system/jsx'
 import { AuthenticationCard } from '~/components/examples/authentication-card'
+import { DatePicker } from '~/components/ui/date-picker'
 
 export default function Home() {
   return (
     <main>
       <Title>Welcome to Park UI</Title>
       <Container py={{ base: '16', md: '24' }}>
+        <DatePicker />
         <AuthenticationCard />
       </Container>
     </main>
